9a7fcf668f271a97a078ddc542e2aa56942a906f
goksukaraca <87966885+goksukaraca@users.noreply.github.com>
2021-08-17 12:02:59 +0300

    link

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import React from 'react';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
-          <Form />
       </header>
     </div>
   );
